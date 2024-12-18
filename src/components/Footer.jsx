3a0b1246d3c445d6b3fae329fab597cb63303c9b
md-ashikur <ashik76690@gmail.com>
2024-12-18 13:48:05 +0600

    footer design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,158 +1,158 @@
-import Image from 'next/image';
-import logo from "../../public/images/2.png"
+import Image from "next/image";
+import logo from "../../public/images/2.png";
 
 const Footer = () => {
   return (
-
-   <div>
+    <div>
       {/* Top Design */}
-      <div className="top-design2 "></div>
- 
-      <div className="top-design1 "></div>
-  
+      <div className="-mb-6">
+        <div className="top-design2 "></div>
 
-    <footer className="bg-primary font-light text-sm text-white py-10 px-20">
-      <div className="container mx-auto px-6 grid grid-cols-1 md:grid-cols-4 gap-8">
-        {/* Logo and Contact */}
-        <div className="space-y-4">
-          <Image src={logo} alt="" className='w-28'/>
-         
-          <p className="text-sm">
-            805 route de Sully 45460 Bray en Val <br />
-            <a
-              href="mailto:contact@loungeandspa.fr"
-              className="hover:underline"
-            >
-              contact@loungeandspa.fr
-            </a>
-            <br />
-            06 43 17 68 68
-          </p>
-          {/* Social Icons */}
-          <div className="flex space-x-4">
-            <a href="#" className="hover:text-gray-300">
-              <i className="fab fa-twitter"></i>
-            </a>
-            <a href="#" className="hover:text-gray-300">
-              <i className="fab fa-facebook"></i>
-            </a>
-            <a href="#" className="hover:text-gray-300">
-              <i className="fab fa-youtube"></i>
-            </a>
-            <a href="#" className="hover:text-gray-300">
-              <i className="fab fa-linkedin"></i>
-            </a>
-          </div>
-        </div>
-
-        {/* Menu */}
-        <div>
-          <h3 className="font-semibold mb-4 text-xl">Menu</h3>
-          <ul className="space-y-2">
-            <li>
-              <a href="#" className="hover:underline">
-                About Us
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline">
-                NOTRE DOCUMENTATION
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline">
-                Contact Us
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline">
-                RÉSERVATION EN LIGNE
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline">
-                CGV
-              </a>
-            </li>
-          </ul>
-        </div>
-
-        {/* Meet Lounge and Spa */}
-        <div>
-          <h3 className="font-semibold mb-4  text-xl">Meet Lounge and Spa</h3>
-          <ul className="space-y-2">
-            <li>
-              <a href="#" className="hover:underline">
-                EVJF
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline">
-                Gallery
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline">
-                FAQ
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline">
-                LOCATION DE SALLE (jusqu&apos;à 80 pers)
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline">
-                ENTREPRISES (jusqu&apos;à 80 pers)
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline">
-                CARTES CADEAUX
-              </a>
-            </li>
-          </ul>
-        </div>
-
-        {/* Instagram */}
-        <div>
-          <h3 className="font-semibold mb-4  text-xl">Instagram</h3>
-          <div className="grid grid-cols-3 gap-2">
-            <Image
-              src=""
-              alt="Instagram 1"
-              className="w-full h-full object-cover"
-            />
-            <Image
-              src=""
-              alt="Instagram 2"
-              className="w-full h-full object-cover"
-            />
-            <Image
-              src=""
-              alt="Instagram 3"
-              className="w-full h-full object-cover"
-            />
-            <Image
-              src=""
-              alt="Instagram 4"
-              className="w-full h-full object-cover"
-            />
-            <Image
-              src=""
-              alt="Instagram 5"
-              className="w-full h-full object-cover"
-            />
-            <Image
-              src=""
-              alt="Instagram 6"
-              className="w-full h-full object-cover"
-            />
-          </div>
-        </div>
+        <div className="top-design1 "></div>
       </div>
-    </footer>
-   </div>
+
+      <footer className="bg-primary font-light text-sm text-white py-10 lg:px-20">
+        <div className="container mx-auto px-6 grid grid-cols-1 md:grid-cols-4 gap-8">
+          {/* Logo and Contact */}
+          <div className="space-y-4">
+            <Image src={logo} alt="" className="w-28" />
+
+            <p className="text-sm">
+              805 route de Sully 45460 Bray en Val <br />
+              <a
+                href="mailto:contact@loungeandspa.fr"
+                className="hover:underline"
+              >
+                contact@loungeandspa.fr
+              </a>
+              <br />
+              06 43 17 68 68
+            </p>
+            {/* Social Icons */}
+            <div className="flex space-x-4">
+              <a href="#" className="hover:text-gray-300">
+                <i className="fab fa-twitter"></i>
+              </a>
+              <a href="#" className="hover:text-gray-300">
+                <i className="fab fa-facebook"></i>
+              </a>
+              <a href="#" className="hover:text-gray-300">
+                <i className="fab fa-youtube"></i>
+              </a>
+              <a href="#" className="hover:text-gray-300">
+                <i className="fab fa-linkedin"></i>
+              </a>
+            </div>
+          </div>
+
+          {/* Menu */}
+          <div>
+            <h3 className="font-semibold mb-4 text-xl">Menu</h3>
+            <ul className="space-y-2">
+              <li>
+                <a href="#" className="hover:underline">
+                  About Us
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline">
+                  NOTRE DOCUMENTATION
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline">
+                  Contact Us
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline">
+                  RÉSERVATION EN LIGNE
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline">
+                  CGV
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          {/* Meet Lounge and Spa */}
+          <div>
+            <h3 className="font-semibold mb-4  text-xl">Meet Lounge and Spa</h3>
+            <ul className="space-y-2">
+              <li>
+                <a href="#" className="hover:underline">
+                  EVJF
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline">
+                  Gallery
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline">
+                  FAQ
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline">
+                  LOCATION DE SALLE (jusqu&apos;à 80 pers)
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline">
+                  ENTREPRISES (jusqu&apos;à 80 pers)
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline">
+                  CARTES CADEAUX
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          {/* Instagram */}
+          <div>
+            <h3 className="font-semibold mb-4  text-xl">Instagram</h3>
+            <div className="grid grid-cols-3 gap-2">
+              <Image
+                src=""
+                alt="Instagram 1"
+                className="w-full h-full object-cover"
+              />
+              <Image
+                src=""
+                alt="Instagram 2"
+                className="w-full h-full object-cover"
+              />
+              <Image
+                src=""
+                alt="Instagram 3"
+                className="w-full h-full object-cover"
+              />
+              <Image
+                src=""
+                alt="Instagram 4"
+                className="w-full h-full object-cover"
+              />
+              <Image
+                src=""
+                alt="Instagram 5"
+                className="w-full h-full object-cover"
+              />
+              <Image
+                src=""
+                alt="Instagram 6"
+                className="w-full h-full object-cover"
+              />
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
   );
 };
 
