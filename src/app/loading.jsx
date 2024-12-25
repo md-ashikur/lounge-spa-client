4046ad0c87f49c0a22f47b190184d066fb1bcc2b
md-ashikur <ashik76690@gmail.com>
2024-12-25 14:20:34 +0600

    loading page

---
+++ src/app/loading.jsx
@@ -0,0 +1,8 @@
+'use client'
+export default function loading() {
+  return (
+    <div className='text-2xl h-screen bg-secondary flex justify-center items-center'>
+      Loading....
+    </div>
+  )
+}
