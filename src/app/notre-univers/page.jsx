40279a9ce8f3fc8b871148602bdcf389571ededa
md-ashikur <ashik76690@gmail.com>
2025-03-04 12:21:42 +0600

    design update notre  page

--- src/app/notre-univers/page.jsx
+++ src/app/notre-univers/page.jsx
@@ -1,10 +1,10 @@
-import OurStory from '@/components/(notre-univers)/UniverseStep1'
+import UniverseStep1 from '@/components/(notre-univers)/UniverseStep1'
 import React from 'react'
 
 export default function page() {
   return (
     <div>
-     <OurStory/>
+     <UniverseStep1/>
     </div>
   )
 }
